04ce5be56e4f08e6fe1dd4aaa20360f7a0f6bc3c
vansi1804 <65750930+vansi1804@users.noreply.github.com>
2021-09-06 22:12:13 +0700

    open  chuc nang doc ghi file

--- src/main/java/schoolmanagement/schoolmanagement/StudentList.java
+++ src/main/java/schoolmanagement/schoolmanagement/StudentList.java
@@ -1,6 +1,6 @@
 package schoolmanagement.schoolmanagement;
 
-// import java.io.*;
+ import java.io.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
@@ -8,7 +8,7 @@ import java.util.Scanner;
 import schoolmanagement.schoolmanagement.Sort.*;
 
 
-public class StudentList{
+public class StudentList implements Serializable{
     private ArrayList<Student> studentlist = new ArrayList<>();
 
     public ArrayList<Student> getStudentlist() {
@@ -77,91 +77,81 @@ public class StudentList{
     }
 
     public String ScholarshipStudentList() {
-        String str = "";
+        String scholarshipStudentList = "";
         for (Student student : studentlist) {
             if (student.hasScholarship()) {
-                str += "\n" + student.toString();
+                scholarshipStudentList += "\n" + student.toString();
             };
         }
-        return str;
+        return scholarshipStudentList;
     }
 
-    public void init() {
-        Add(new Student("A", "1", "1/1/1", "1D", "111", 1, "1@"));
-        Add(new Student("B", "0", "1/2/2", "2D", "222", (float) 8.5, "1@"));
-        Add(new Student("3", "1", "1/2/3", "3D", "333", 4, "1@"));
-        Add(new Student("D", "1", "2/2/2", "4D", "444", 8, "1@"));
-        Add(new Student("2", "0", "2/3/3", "5D", "555", 5, "1@"));
-        Add(new Student("C", "0", "2/3/4", "6D", "666", 7, "1@"));
-        Add(new Student("7", "0", "3/3/3", "7D", "777", 3, "1@"));
-        Add(new Student("H", "1", "3/4/4", "8", "888", 9, "1@"));
-        Add(new Student("1", "1", "3/4/5", "9D", "999", 0, "1@"));
-        Add(new Student("E", "1", "4/4/4", "10D", "101", 10, "1@"));
+    void writeDataToFile(String filename) {
+        FileOutputStream fos = null;
+        ObjectOutputStream oos = null;
+
+        try {
+            fos = new FileOutputStream(filename);
+            oos = new ObjectOutputStream(fos);
+            oos.writeObject(studentlist);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (oos != null) {
+                try {
+                    oos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
-    // void writeDataToFile(String filename) {
-    //     FileOutputStream fos = null;
-    //     ObjectOutputStream oos = null;
 
-    //     try {
-    //         fos = new FileOutputStream(filename);
-    //         oos = new ObjectOutputStream(fos);
+    void readDataFromFile(String filename) {
+        FileInputStream fis = null;
+        ObjectInputStream ois = null;
 
-    //         oos.writeObject(studentlist);
-    //     } catch (FileNotFoundException e) {
-    //         e.printStackTrace();
-    //     } catch (IOException e) {
-    //         e.printStackTrace();
-    //     } finally {
-    //         if (fos != null) {
-    //             try {
-    //                 fos.close();
-    //             } catch (IOException e) {
-    //                 e.printStackTrace();
-    //             }
-    //         }
-    //         if (oos != null) {
-    //             try {
-    //                 oos.close();
-    //             } catch (IOException e) {
-    //                 e.printStackTrace();
-    //             }
-    //         }
-    //     }
-    // }
+        try {
+            fis = new FileInputStream(filename);
+            ois = new ObjectInputStream(fis);
 
-    // void readDataFromFile(String filename) {
-    //     FileInputStream fis = null;
-    //     ObjectInputStream ois = null;
-
-    //     try {
-    //         fis = new FileInputStream(filename);
-    //         ois = new ObjectInputStream(fis);
-
-    //         setStudentlist((ArrayList<Student>) ois.readObject());
+            setStudentlist((ArrayList<Student>) ois.readObject());
+            for (Student student : studentlist) {
+                System.out.print(student);
+            }
         
-    //     } catch (FileNotFoundException e) {
-    //         e.printStackTrace();
-    //     } catch (IOException e) {
-    //         e.printStackTrace();
-    //     } catch (ClassNotFoundException e) {
-    //         e.printStackTrace();
-    //     } finally {
-    //         if (fis != null) {
-    //             try {
-    //                 fis.close();
-    //             } catch (IOException e) {
-    //                 e.printStackTrace();
-    //             }
-    //         }
-    //         if (ois != null) {
-    //             try {
-    //                 ois.close();
-    //             } catch (IOException e) {
-    //                 e.printStackTrace();
-    //             }
-    //         }
-    //     }
-    // }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            if (fis != null) {
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (ois != null) {
+                try {
+                    ois.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 
 }
--- src/main/java/schoolmanagement/schoolmanagement/StudentMAIN.java
+++ src/main/java/schoolmanagement/schoolmanagement/StudentMAIN.java
@@ -1,5 +1,6 @@
 package schoolmanagement.schoolmanagement;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class StudentMAIN {
@@ -7,8 +8,7 @@ public class StudentMAIN {
     static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
         StudentList studentList = new StudentList();
-        studentList.init();
-        studentList.Show();
+        System.out.println("\n\nBan đầu danh sách rỗng, hãy khởi tạo 1 danh sách, xem danh sách, lưu file và đọc file");
         while (true) {
             int choose = 0;
             boolean check = false;
@@ -42,18 +42,22 @@ public class StudentMAIN {
                     break;
 
                 case 4: studentList.Sort();
-                    System.out.print("Student list is sorted. ");
+                        System.out.print("->>>>>Student list is sorted. ");
                     break;
 
                 case 5:
                         System.out.println(studentList.ScholarshipStudentList());
                     break;
 
-                // case 6: studentList.writeDataToFile("studentlist.dat");
-                //     break;
+                case 6: studentList.writeDataToFile("studentlist.dat");
+                    break;
 
-                // case 7: studentList.readDataFromFile("studentlist.dat");
-                //     break;
+                case 7: studentList.readDataFromFile("studentlist.dat");
+                    break;
+                
+                case 8: studentList.setStudentlist(initStudentList());
+                        System.out.print("->>>>>A Student list is init. ");
+                    break;
 
                 default:    System.err.println("\n\n\t\t\t\t\tChoice is not legal");
                     break;
@@ -71,9 +75,24 @@ public class StudentMAIN {
         System.out.println("\t\t\t\t\t\t\t3.Find student");
         System.out.println("\t\t\t\t\t\t\t4.Sort student list");
         System.out.println("\t\t\t\t\t\t\t5.Show student's information who has scholarship");
-        // System.out.println("\t\t\t\t\t\t\t6.Save to file.");
-        // System.out.println("\t\t\t\t\t\t\t7.Read from file.");
+        System.out.println("\t\t\t\t\t\t\t6.Save file.");
+        System.out.println("\t\t\t\t\t\t\t7.Read file.");
+        System.out.println("\t\t\t\t\t\t\t8.Init a student list.");
         System.out.print("\t\t\t\t\t\t\t\tChoose? __");
     }
-    
+
+    public static ArrayList<Student> initStudentList() {
+        ArrayList<Student> list = new ArrayList<>();
+        list.add(new Student("A", "1", "1/1/1", "1D", "111", 1, "1@"));
+        list.add(new Student("B", "0", "1/2/2", "2D", "222", (float) 8.5, "1@"));
+        list.add(new Student("3", "1", "1/2/3", "3D", "333", 4, "1@"));
+        list.add(new Student("D", "1", "2/2/2", "4D", "444", 8, "1@"));
+        list.add(new Student("2", "0", "2/3/3", "5D", "555", 5, "1@"));
+        list.add(new Student("C", "0", "2/3/4", "6D", "666", 7, "1@"));
+        list.add(new Student("7", "0", "3/3/3", "7D", "777", 3, "1@"));
+        list.add(new Student("H", "1", "3/4/4", "8", "888", 9, "1@"));
+        list.add(new Student("1", "1", "3/4/5", "9D", "999", 0, "1@"));
+        list.add(new Student("E", "1", "4/4/4", "10D", "101", 10, "1@"));
+        return list;
+    }
 }
